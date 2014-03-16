3200e387fb3b154931d3c676ea576b64f16ea66f
Phil <sittipol_tribunyatkul@hotmail.com>
2014-03-16 11:12:59 -0700

    Revert "broken android side"
    This reverts commit 6af6ee24677bfb0fec079f0fb48fdd697ff45429.

--- android/src/com/example/ece381/MainActivity.java
+++ android/src/com/example/ece381/MainActivity.java
@@ -8,8 +8,6 @@ import java.sql.Timestamp;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import com.example.ece381.MyApplication.TcpData;
-
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -20,7 +18,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends MyActivity {
+public class MainActivity extends Activity {
 
 	//stuff I wrote
 	//speed test and latency test
@@ -74,13 +72,12 @@ public class MainActivity extends MyActivity {
 		}
 	}
 	
-	
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		try{
+		
 		// This call will result in better error messages if you
 		// try to do things in the wrong thread.
+		
 		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
 				.detectDiskReads().detectDiskWrites().detectNetwork()
 				.penaltyLog().build());
@@ -92,9 +89,17 @@ public class MainActivity extends MyActivity {
 		et.setKeyListener(null);
 		et = (EditText) findViewById(R.id.error_message_box);
 		et.setKeyListener(null);
-		}catch(NullPointerException E){
-			Log.i("MAIN","wtf",E);
-		}
+
+		Thread TCP_read = new TCPReadThread();
+		TCP_read.start();
+		
+		
+		// Set up a timer task.  We will use the timer to check the
+		// input queue every 500 ms
+		
+		//TCPReadTimerTask tcp_task = new TCPReadTimerTask();
+		//Timer tcp_timer = new Timer();
+		//tcp_timer.schedule(tcp_task, 3000, 500);
 	}
 
 	@Override
@@ -104,6 +109,7 @@ public class MainActivity extends MyActivity {
 	}
 
 	// Route called when the user presses "connect"
+	
 	public void openSocket(View view) {
 		MyApplication app = (MyApplication) getApplication();
 		TextView msgbox = (TextView) findViewById(R.id.error_message_box);
@@ -123,42 +129,58 @@ public class MainActivity extends MyActivity {
 	}
 
 	//  Called when the user wants to send a message
+	
 	public void sendMessage(View view) {
 		
 		MyApplication app = (MyApplication) getApplication();
 		
 		if(!app.ack) return;
-		if (app.sock != null && app.sock.isConnected() && !app.sock.isClosed()) {
-			// Get the message from the box
-			
-			EditText et = (EditText) findViewById(R.id.MessageText);
-			String msg = et.getText().toString();
-	
-			// Create an array of bytes.  First byte will be the
-			// message length, and the next ones will be the message
-			
-			byte buf[] = new byte[msg.length()+1];
-			buf[0] = 1; //data type 1
-			System.arraycopy(msg.getBytes(), 0, buf, 1, msg.length());
-	
-			// Now send through the output stream of the socket
-			OutputStream out;
+		
+		// Get the message from the box
+		
+		EditText et = (EditText) findViewById(R.id.MessageText);
+		String msg = et.getText().toString();
+
+		// Create an array of bytes.  First byte will be the
+		// message length, and the next ones will be the message
+		
+		byte buf[] = new byte[msg.length()+1];
+		buf[0] = 1; //data type 1
+		System.arraycopy(msg.getBytes(), 0, buf, 1, msg.length());
+
+		// Now send through the output stream of the socket
+		OutputStream out;
+		try {
+			out = app.sock.getOutputStream();
 			try {
-				out = app.sock.getOutputStream();
-				try {
-					out.write(buf, 0, msg.length()+1);
-					app.ack = false;
-					Log.i("ack","false");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+				out.write(buf, 0, msg.length()+1);
+				app.ack = false;
+				Log.i("ack","false");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	// Called when the user closes a socket
+	
+	public void closeSocket(View view) {
+		MyApplication app = (MyApplication) getApplication();
+		app.ack = true;
+		Socket s = app.sock;
+		try {
+			s.getOutputStream().close();
+			s.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
 	// Construct an IP address from the four boxes
+	
 	public String getConnectToIP() {
 		String addr = "";
 		EditText text_ip;
@@ -174,6 +196,7 @@ public class MainActivity extends MyActivity {
 	}
 
 	// Gets the Port from the appropriate field.
+	
 	public Integer getConnectToPort() {
 		Integer port;
 		EditText text_port;
@@ -189,6 +212,7 @@ public class MainActivity extends MyActivity {
 	// has to be done in an Asynchronous thread in Android.  Be sure you
 	// have done the Asynchronous Tread tutorial before trying to understand
 	// this code.
+	
 	public class SocketConnect extends AsyncTask<Void, Void, Socket> {
 
 		// The main parcel of work for this thread.  Opens a socket
@@ -220,10 +244,67 @@ public class MainActivity extends MyActivity {
 		}
 	}
 
-
-	@Override
-	public void handlesTCPData(TcpData d) {
-		// TODO Auto-generated method stub
+	// This is a timer Task.  Be sure to work through the tutorials
+	// on Timer Tasks before trying to understand this code.
+	
+	public class TCPReadThread extends Thread {
+		//data
+		private byte buffer[] = new byte[256];
+		private byte dataType;
+		private int size;
 		
+		@Override
+		public void run() {
+			MyApplication app = (MyApplication) getApplication();
+			while(!(app.sock != null && app.sock.isConnected() && !app.sock.isClosed()));
+				
+			try {
+				InputStream in = app.sock.getInputStream();
+
+
+				while(true) {
+					// See if any bytes are available from the Middleman
+					size = (byte) in.read();
+					if(size == 0) {
+						app.ack = true;
+						Log.i("ack","true");
+					} else{
+						dataType = (byte) in.read();
+						
+						int amount = in.read(buffer,0,size-1);
+						Log.i("COMM", "Data Received -> Expected: " + String.valueOf(size-1) + " Got: " + String.valueOf(amount));
+						//read the rest of the data if it wasn't read in properly
+						if(amount < size-1) {
+							for(int i = amount; i < size; i++) {
+								buffer[i] = (byte)in.read();
+							}
+						}
+						
+						switch(dataType){
+							case 0:
+								//speedtest
+								//do nothing
+								break;
+							case 1:
+								// If so, read them in and create a sring
+								final String s = new String(buffer, 0, size-1, "US-ASCII");
+				
+								// As explained in the tutorials, the GUI can not be
+								// updated in an asyncrhonous task.  So, update the GUI
+								// using the UI thread.
+								runOnUiThread(new Runnable() {
+									public void run() {
+										EditText et = (EditText) findViewById(R.id.RecvdMessage);
+										et.setText(s);
+									}
+								});
+								break;
+						}
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- android/src/com/example/ece381/MyApplication.java
+++ android/src/com/example/ece381/MyApplication.java
@@ -4,50 +4,42 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 
-import android.app.Activity;
 import android.app.Application;
 import android.util.Log;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MyApplication extends Application {
-	///////////////////////////
-	//Static Application Data//
-	///////////////////////////
-	Socket sock = null; //is socket open?
-	boolean ack = true; //has the sent packet been read?
-	TCPReadThread tcp_read;
-	Activity currentActivity;
-	TextView currentTextView;
-	
-	Object lock;
+	Socket sock = null;
+	boolean ack = true;
+	Thread TCPread;
 	
 	@Override
 	public void onCreate(){
 		super.onCreate();
-		tcp_read = new TCPReadThread();
-		tcp_read.start();
+		
 	}
 	
-	public class TcpData {
-		public byte dataType;
-		public byte data[];
-	}
-
+	// This is a timer Task.  Be sure to work through the tutorials
+	// on Timer Tasks before trying to understand this code.
+	/*
 	public class TCPReadThread extends Thread {
 		//data
-		private byte buffer[] = new byte[512];
-		private byte dataType;
-		private int size;
+		public byte buffer[] = new byte[256];
+		public byte dataType;
+		public int  size;
+		
+		//data
+		public byte buffer[] = new byte[256];
+		public byte dataType;
+		public int  size;
 		
 		@Override
 		public void run() {
-			//wait for socket to be connected
+
 			while(!(sock != null && sock.isConnected() && !sock.isClosed()));
 				
 			try {
 				InputStream in = sock.getInputStream();
-
 
 				while(true) {
 					// See if any bytes are available from the Middleman
@@ -57,47 +49,13 @@ public class MyApplication extends Application {
 						Log.i("ack","true");
 					} else{
 						dataType = (byte) in.read();
-						size--;
 						
-						int amount = in.read(buffer,0,size);
-						Log.i("COMM", "Data Received -> Expected: " + String.valueOf(size) + " Got: " + String.valueOf(amount));
+						int amount = in.read(buffer,0,size-1);
+						Log.i("COMM", "Data Received -> Expected: " + String.valueOf(size-1) + " Got: " + String.valueOf(amount));
 						//read the rest of the data if it wasn't read in properly
 						if(amount < size) {
 							for(int i = amount; i < size; i++) {
 								buffer[i] = (byte)in.read();
-							}
-						}
-						
-						TcpData d = new TcpData();
-						d.data = new byte[size];
-						int i;
-						for(i=0;i<size;i++){
-							d.data[i] = buffer[i];
-						}
-						d.dataType = dataType;
-						
-						synchronized(lock) {
-							if(currentActivity != null) {
-								switch(dataType){
-									case 0:
-										//speedtest
-										//do nothing
-										break;
-									case 1:
-										// If so, read them in and create a sring
-										final String s = new String(buffer, 0, size-1, "US-ASCII");
-						
-										// As explained in the tutorials, the GUI can not be
-										// updated in an asyncrhonous task.  So, update the GUI
-										// using the UI thread.
-										currentActivity.runOnUiThread(new Runnable() {
-											public void run() {
-												EditText et = (EditText) currentActivity.findViewById(R.id.RecvdMessage);
-												et.setText(s);
-											}
-										});
-										break;
-								}
 							}
 						}
 					}
@@ -106,5 +64,5 @@ public class MyApplication extends Application {
 				e.printStackTrace();
 			}
 		}
-	}
+	}*/
 }
