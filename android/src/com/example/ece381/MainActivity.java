6af6ee24677bfb0fec079f0fb48fdd697ff45429
Phil <sittipol_tribunyatkul@hotmail.com>
2014-03-16 11:12:48 -0700

    broken android side

--- android/src/com/example/ece381/MainActivity.java
+++ android/src/com/example/ece381/MainActivity.java
@@ -8,6 +8,8 @@ import java.sql.Timestamp;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import com.example.ece381.MyApplication.TcpData;
+
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -18,7 +20,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends MyActivity {
 
 	//stuff I wrote
 	//speed test and latency test
@@ -72,12 +74,13 @@ public class MainActivity extends Activity {
 		}
 	}
 	
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		
+		try{
 		// This call will result in better error messages if you
 		// try to do things in the wrong thread.
-		
 		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
 				.detectDiskReads().detectDiskWrites().detectNetwork()
 				.penaltyLog().build());
@@ -89,17 +92,9 @@ public class MainActivity extends Activity {
 		et.setKeyListener(null);
 		et = (EditText) findViewById(R.id.error_message_box);
 		et.setKeyListener(null);
-
-		Thread TCP_read = new TCPReadThread();
-		TCP_read.start();
-		
-		
-		// Set up a timer task.  We will use the timer to check the
-		// input queue every 500 ms
-		
-		//TCPReadTimerTask tcp_task = new TCPReadTimerTask();
-		//Timer tcp_timer = new Timer();
-		//tcp_timer.schedule(tcp_task, 3000, 500);
+		}catch(NullPointerException E){
+			Log.i("MAIN","wtf",E);
+		}
 	}
 
 	@Override
@@ -109,7 +104,6 @@ public class MainActivity extends Activity {
 	}
 
 	// Route called when the user presses "connect"
-	
 	public void openSocket(View view) {
 		MyApplication app = (MyApplication) getApplication();
 		TextView msgbox = (TextView) findViewById(R.id.error_message_box);
@@ -129,58 +123,42 @@ public class MainActivity extends Activity {
 	}
 
 	//  Called when the user wants to send a message
-	
 	public void sendMessage(View view) {
 		
 		MyApplication app = (MyApplication) getApplication();
 		
 		if(!app.ack) return;
-		
-		// Get the message from the box
-		
-		EditText et = (EditText) findViewById(R.id.MessageText);
-		String msg = et.getText().toString();
-
-		// Create an array of bytes.  First byte will be the
-		// message length, and the next ones will be the message
-		
-		byte buf[] = new byte[msg.length()+1];
-		buf[0] = 1; //data type 1
-		System.arraycopy(msg.getBytes(), 0, buf, 1, msg.length());
-
-		// Now send through the output stream of the socket
-		OutputStream out;
-		try {
-			out = app.sock.getOutputStream();
+		if (app.sock != null && app.sock.isConnected() && !app.sock.isClosed()) {
+			// Get the message from the box
+			
+			EditText et = (EditText) findViewById(R.id.MessageText);
+			String msg = et.getText().toString();
+	
+			// Create an array of bytes.  First byte will be the
+			// message length, and the next ones will be the message
+			
+			byte buf[] = new byte[msg.length()+1];
+			buf[0] = 1; //data type 1
+			System.arraycopy(msg.getBytes(), 0, buf, 1, msg.length());
+	
+			// Now send through the output stream of the socket
+			OutputStream out;
 			try {
-				out.write(buf, 0, msg.length()+1);
-				app.ack = false;
-				Log.i("ack","false");
+				out = app.sock.getOutputStream();
+				try {
+					out.write(buf, 0, msg.length()+1);
+					app.ack = false;
+					Log.i("ack","false");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
-	// Called when the user closes a socket
-	
-	public void closeSocket(View view) {
-		MyApplication app = (MyApplication) getApplication();
-		app.ack = true;
-		Socket s = app.sock;
-		try {
-			s.getOutputStream().close();
-			s.close();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 
 	// Construct an IP address from the four boxes
-	
 	public String getConnectToIP() {
 		String addr = "";
 		EditText text_ip;
@@ -196,7 +174,6 @@ public class MainActivity extends Activity {
 	}
 
 	// Gets the Port from the appropriate field.
-	
 	public Integer getConnectToPort() {
 		Integer port;
 		EditText text_port;
@@ -212,7 +189,6 @@ public class MainActivity extends Activity {
 	// has to be done in an Asynchronous thread in Android.  Be sure you
 	// have done the Asynchronous Tread tutorial before trying to understand
 	// this code.
-	
 	public class SocketConnect extends AsyncTask<Void, Void, Socket> {
 
 		// The main parcel of work for this thread.  Opens a socket
@@ -244,67 +220,10 @@ public class MainActivity extends Activity {
 		}
 	}
 
-	// This is a timer Task.  Be sure to work through the tutorials
-	// on Timer Tasks before trying to understand this code.
-	
-	public class TCPReadThread extends Thread {
-		//data
-		private byte buffer[] = new byte[256];
-		private byte dataType;
-		private int size;
+
+	@Override
+	public void handlesTCPData(TcpData d) {
+		// TODO Auto-generated method stub
 		
-		@Override
-		public void run() {
-			MyApplication app = (MyApplication) getApplication();
-			while(!(app.sock != null && app.sock.isConnected() && !app.sock.isClosed()));
-				
-			try {
-				InputStream in = app.sock.getInputStream();
-
-
-				while(true) {
-					// See if any bytes are available from the Middleman
-					size = (byte) in.read();
-					if(size == 0) {
-						app.ack = true;
-						Log.i("ack","true");
-					} else{
-						dataType = (byte) in.read();
-						
-						int amount = in.read(buffer,0,size-1);
-						Log.i("COMM", "Data Received -> Expected: " + String.valueOf(size-1) + " Got: " + String.valueOf(amount));
-						//read the rest of the data if it wasn't read in properly
-						if(amount < size-1) {
-							for(int i = amount; i < size; i++) {
-								buffer[i] = (byte)in.read();
-							}
-						}
-						
-						switch(dataType){
-							case 0:
-								//speedtest
-								//do nothing
-								break;
-							case 1:
-								// If so, read them in and create a sring
-								final String s = new String(buffer, 0, size-1, "US-ASCII");
-				
-								// As explained in the tutorials, the GUI can not be
-								// updated in an asyncrhonous task.  So, update the GUI
-								// using the UI thread.
-								runOnUiThread(new Runnable() {
-									public void run() {
-										EditText et = (EditText) findViewById(R.id.RecvdMessage);
-										et.setText(s);
-									}
-								});
-								break;
-						}
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 }
